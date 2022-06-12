f2dfb3f5d6d0fd0ae7e89810459fc89cc5f0a76e
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-06-12 14:37:54 -0500

    Add files via upload

--- Etch-a-sketch/main.py
+++ Etch-a-sketch/main.py
@@ -5,7 +5,17 @@ screen = Screen()
 
 def move_forward():
     turtle.forward(10)
+def move_backward():
+    turtle.backward(10)
+def turn_left():
+    turtle.left(7)
+def turn_right():
+    turtle.right(7)
 
 screen.listen()
-screen.onkey(fun=move_forward, key="space")
+screen.onkey(fun=move_forward, key="w")
+screen.onkey(fun=move_backward, key="s")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=turn_right, key="d")
+
 screen.exitonclick()
